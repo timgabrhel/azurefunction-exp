2204f77c663989d6d2bb490dd1b0e0884e6d726a
Tim Gabrhel <timgabrhel@gmail.com>
2016-04-01 13:34:14 -0500

    add new manual read queue

---
+++ Manual-ReadQueue/index.js
@@ -0,0 +1,6 @@
+﻿module.exports = function (context, input, myTable) {
+    context.log('Node.js manually triggered function called with input', input);
+    context.log(JSON.stringify(myTable));
+    context.log(JSON.stringify(context));
+    context.done();
+};
